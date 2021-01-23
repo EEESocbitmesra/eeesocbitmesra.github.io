353fe846868f430ff38a9d58569f6751e4870741
Devansh Baldwa <devanshbaldwa@gmail.com>
2021-01-24 04:23:24 +0530

    First Commit.

---
+++ precache-manifest.2b9a49857002b2923b01ec824665ab83.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97d32b4a2deb413b98ccd9efe8e5712",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf71e786505e46e4cb46",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f9ac908cad4b189e0d82",
+    "url": "./static/css/main.8e4d1c5d.chunk.css"
+  },
+  {
+    "revision": "bf71e786505e46e4cb46",
+    "url": "./static/js/2.76d5715a.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "./static/js/2.76d5715a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ac908cad4b189e0d82",
+    "url": "./static/js/main.69ceb966.chunk.js"
+  },
+  {
+    "revision": "ffdd36f53910a8f45474",
+    "url": "./static/js/runtime-main.c68a49cd.js"
+  },
+  {
+    "revision": "cf28632239db6e02bc09bcca6bf5360f",
+    "url": "./static/media/Matlab.cf286322.png"
+  },
+  {
+    "revision": "49092ede7f14ebf0e87897e9d33f514d",
+    "url": "./static/media/amrit_bhaiya_k17.49092ede.jpeg"
+  },
+  {
+    "revision": "a0f641943fd0c4056007b1fcff4acb81",
+    "url": "./static/media/banner3.a0f64194.jpg"
+  },
+  {
+    "revision": "8b0405c454a92d3a46e77853740860c5",
+    "url": "./static/media/bit_mesra_img.8b0405c4.png"
+  },
+  {
+    "revision": "ceb4d833ecdaa4cd317106ef39a172ef",
+    "url": "./static/media/bluetooth_bot.ceb4d833.png"
+  },
+  {
+    "revision": "074ca123f6f3498730773167211396cf",
+    "url": "./static/media/c.074ca123.jpg"
+  },
+  {
+    "revision": "af58500fa430f40753d0f251e8485686",
+    "url": "./static/media/eee_about_page1.af58500f.png"
+  },
+  {
+    "revision": "4eb340580a5f6bc19aa074ffad320d81",
+    "url": "./static/media/hitesh.4eb34058.jpg"
+  },
+  {
+    "revision": "3e59bbd4ac493e96f43c9e1390aa23d8",
+    "url": "./static/media/home_automation.3e59bbd4.png"
+  },
+  {
+    "revision": "2c49e27b2080f668dbc44b210461ee13",
+    "url": "./static/media/mathworks_logo.2c49e27b.jpg"
+  },
+  {
+    "revision": "984299300d04243d2e893a0aef139877",
+    "url": "./static/media/matlab_workshop1.98429930.png"
+  },
+  {
+    "revision": "d319b596536286ed405a0688d6c09bd9",
+    "url": "./static/media/mitra_di_k17.d319b596.jpeg"
+  },
+  {
+    "revision": "cafa47675f338101e197d124db583687",
+    "url": "./static/media/naman_bhaiya_k17.cafa4767.jpg"
+  },
+  {
+    "revision": "7a50d593c2b78b79c0f73f22e72772f4",
+    "url": "./static/media/naveen_bhaiya_k17.7a50d593.jpg"
+  },
+  {
+    "revision": "6e9ebff6e697c1e11ff10c5c390810f1",
+    "url": "./static/media/nishant.6e9ebff6.jpg"
+  },
+  {
+    "revision": "c38edc1a837b1bcea0788163a0e02478",
+    "url": "./static/media/ocr.c38edc1a.png"
+  },
+  {
+    "revision": "c433f2b0476cd288b1023c3517e8edd2",
+    "url": "./static/media/power_electronics.c433f2b0.jpg"
+  },
+  {
+    "revision": "ab77a97f28b76917ce36b4ed0b7fbced",
+    "url": "./static/media/prabhat_bhaiya_k17.ab77a97f.jpg"
+  },
+  {
+    "revision": "560dd795261d10f4d5af19b78435e151",
+    "url": "./static/media/project_programme.560dd795.jpg"
+  },
+  {
+    "revision": "13416da804ae226038ff9d5851479046",
+    "url": "./static/media/saujanya_bhaiya_k17.13416da8.jpg"
+  },
+  {
+    "revision": "cb9cabf1cc8d9ff573d2712e535a719d",
+    "url": "./static/media/street_light.cb9cabf1.png"
+  },
+  {
+    "revision": "a396afa6f831ddf76ae1144e214a5277",
+    "url": "./static/media/vivek_bhaiya_k17.a396afa6.jpg"
+  }
+]);
